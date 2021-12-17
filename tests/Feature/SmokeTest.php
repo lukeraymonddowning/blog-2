3fb1f27c5e19893c64868672f058ee2e67518cb4
Luke Downing <lukeraymonddowning@gmail.com>
2021-12-17 15:30:58 +0000

    Adds smoke tests

---
+++ tests/Feature/SmokeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Database\Factories\WinkPostFactory;
+use Wink\WinkPost;
+
+beforeEach(function () {
+    WinkPostFactory::new()->count(3)->create();
+});
+
+it('loads the home page', function () {
+    $this->get(route('home'))->assertOk();
+});
+
+it('loads the posts index page', function () {
+    $this->get(route('posts.index'))->assertOk();
+});
+
+it('loads the posts show page', function () {
+    $this->get(route('posts.show', WinkPost::query()->first()))->assertOk();
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,9 +6,17 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use LazilyRefreshDatabase;
+
+    protected function afterRefreshingDatabase(): void
+    {
+        Artisan::call('migrate', [
+            '--path' => 'vendor/themsaid/wink/src/Migrations'
+        ]);
+    }
 }

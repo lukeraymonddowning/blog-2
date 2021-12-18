ecbd79cdb2ee226fd561d21914c04d6aaea09006
Luke Downing <lukeraymonddowning@gmail.com>
2021-12-18 13:57:05 +0000

    Adds twitter embed script

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,5 @@ require('laravel-mix')
     .js("resources/js/app.js", "public/js")
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
-    ]);
+    ])
+    .version()
